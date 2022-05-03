a30975a3039ce36bc6e8f68ef5eb6c68a0d9b814
seungmin-dev <seungmin.dev@gmail.com>
2022-05-03 11:55:57 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import {createStore} from "redux";
+import { legacy_createStore as createStore } from "redux";
 // store는 state를 저장하는 공간
 // state는 내 application에서 바뀌는 data
 // redux는 내 data를 관리해주기 위해 만들어짐
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// reducer는 내 data를 modify하는 함수
-const reducer = () => {};
-const store = createStore(reducer);
+// reducer(countModifier)는 내 data를 modify하는 함수
+const countModifier = (count = 0) => {
+  return count;
+};
+const countStore = createStore(countModifier);
 
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState());
