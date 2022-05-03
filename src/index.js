9b717354bb57556afc4f34ff572c6beda3330ef8
seungmin-dev <seungmin.dev@gmail.com>
2022-05-03 15:10:00 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -7,9 +7,24 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 // reducer(countModifier)는 내 data를 modify하는 함수
-const countModifier = (count = 0) => {
-  return count;
+// action : redux에서 function을 부를 때 쓰는 두번째 parameter, argument
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" }); // object only
+
+// data의 store를 만들고
+// data의 modifier를 만듦
+// dispatch를 이용해 message를 보냄
+// 보낸 message를 action에 넣고
+// action을 체크해줌
 
 console.log(countStore.getState());
