c88cf622129c7a67a4ba9b4fdc859852a3ad5ed6
seungmin-dev <seungmin.dev@gmail.com>
2022-05-05 23:53:18 +0900

    #2.0 Vanilla To Do - 1

--- src/index.js
+++ src/index.js
@@ -1,43 +1,18 @@
-import { legacy_createStore as createStore } from "redux";
-// store는 state를 저장하는 공간
-// state는 내 application에서 바뀌는 data
-// redux는 내 data를 관리해주기 위해 만들어짐
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-// reducer(countModifier)는 내 data를 modify하는 함수
-// action : redux에서 function을 부를 때 쓰는 두번째 parameter, argument
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
-};
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerHTML = countStore.getState();
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
 };
 
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
