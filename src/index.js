6385004e278296cc7483a12f3d3c447ee794c97b
seungmin-dev <seungmin.dev@gmail.com>
2022-05-06 17:05:40 +0900

    #2.2 Delete To Do

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      // state mutation
+      // 상태를 직접적으로 수정하지 말고 새로운 상태를 return 해야 함
+      // return state.push(action.text); <- 절대 해서는 안되는 행동!
+      return [...state, { text: action.text, id: action.id }];
     case DELETE_TODO:
       return [];
     default:
@@ -20,11 +23,13 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  store.dispatch({ type: ADD_TODO, text: toDo, id: Date.now() });
 };
 
 form.addEventListener("submit", onSubmit);
